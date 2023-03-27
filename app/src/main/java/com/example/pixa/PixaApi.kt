2280f69cf485dda5a694e0c580d95c78bc52bfc3
alibekbajterekov <alibekbajterekov@gmail.com>
2023-03-27 15:35:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/pixa/PixaApi.kt
@@ -0,0 +1,17 @@
+package com.example.pixa
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixaApi {
+
+    @GET("api/")
+    fun getImage(
+        @Query("q") keyWord : String,
+        @Query("key") key : String = "33160454-b52a139941d6e95924e40889a",
+        @Query("per_page") per_page : Int = 3,
+        @Query("page") page : Int = 1
+    ): Call<PixaModel>
+
+}
